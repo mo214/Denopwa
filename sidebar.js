d100c2905c112d1061dd3eba85883f4780642964
Mohammed Saleh <mohammed_500@outlook.dk>
2025-05-25 02:37:28 +0200

    update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,15 +1,15 @@
 function openSidebar() {
-  document.getElementById('sidebar').style.width = '250px';
+  document.getElementByclass('sidebar').style.width = '250px';
 }
 
 function closeSidebar() {
-  document.getElementById('sidebar').style.width = '0';
+  document.getElementByclass('sidebar').style.width = '0';
 }
 
 globalThis.addEventListener('DOMContentLoaded', () => {
   // Optionally, close sidebar when clicking outside of it
   document.addEventListener('click', function(event) {
-    const sidebar = document.getElementById('sidebar');
+    const sidebar = document.getElementByclass('sidebar');
     if (sidebar && sidebar.style.width === '250px' && !sidebar.contains(event.target) && event.target.id !== 'openSidebarBtn') {
       closeSidebar();
     }
