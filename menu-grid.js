e9ca18539849f3646dd7b92b6f1722f439b66339
Mohammed Saleh <mohammed_500@outlook.dk>
2025-06-01 01:22:02 +0200

    update css and menu.html

--- menu-grid.js
+++ menu-grid.js
@@ -1,5 +1,18 @@
 
 function toggleOverlay(elem) {
   const menuItem = elem.closest('.menu-item');
-  menuItem.classList.toggle('show-overlay');
+  if (menuITem) {
+    menuItem.classList.toggle('show-overlay');
+  }
+  else {
+    console.error('Element not found or does not have the expected class.');
+  }
 }
+globalThis.addEventListener('DOMContentLoaded', () => {
+ const  triggerElements = document.querySelectorAll('.js-toggle-overlay-trigger');
+ triggerElements.foreach(triggerElements => {
+  triggerElements.addEventListener('click', () => {toggleOverlay(this);
+  });
+ });
+  });
+ 
