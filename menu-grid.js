6b997e2942a2c57bf6475ed63160721d2b5ffaca
Mohammed Saleh <mohammed_500@outlook.dk>
2025-06-01 19:56:59 +0200

    update

--- menu-grid.js
+++ menu-grid.js
@@ -10,14 +10,14 @@ function toggleOverlay(elem) {
 }
 globalThis.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.menu-item').forEach(el => {
-    el.addEventListener('click', (event) => {
+    el.addEventListener('click', (e) => {
       e.stopPropagation(); // Prevent click from bubbling up to the document
       el.item.classList.toggle('show-overlay');
     });
   });
 
   document.querySelectorAll('.close-btn').forEach(btn => {
-    btn.addEventListener('click', (event) => {
+    btn.addEventListener('click', (e) => {
       e.stopPropagation(); // Prevent click from bubbling up to the document
       const item = e.currentTarget.closest('.menu-item');
       item.classList.remove('show-overlay');
